3aa64d2027c11e8b5b618c9595e922ec7c33b991
isthistaken2 <andrewke20121209@gmail.com>
2022-08-16 09:24:00 -0400

    test

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ var mousetrap1 = document.getElementById('mousetrap-1');
 mousetrap1.onmouseover = function() {
   alert("THIS IS AN ALERT: ANDREW LOVES USING THE WORD TEST");
 }
+
+var mousetrap2 = document.getElementById('mousetrap-2');
+
+mousetrap2.onmouseover = function() {
+  alert("THIS IS AN ALERT: ANDREW LOVES USING THE WORD TEST");
+}
+
+var mousetrap3 = document.getElementById('mousetrap-3');
+
+mousetrap3.onmouseover = function() {
+  alert("THIS IS AN ALERT: ANDREW LOVES USING THE WORD TEST");
+}
+
+var mousetrap4 = document.getElementById('mousetrap-4');
+
+mousetrap4.onmouseover = function() {
+  alert("THIS IS AN ALERT: ANDREW LOVES USING THE WORD TEST");
+}
